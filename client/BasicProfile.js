c4c67eb6ee00287f2cb233e089484f4c9969af1a
Christine Ting <christ123092@yahoo.com>
2020-04-01 22:04:05 -0700

    added styling, camera icon

--- client/BasicProfile.js
+++ client/BasicProfile.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import zipcodes from 'zipcodes';
-import Icon from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 
 import {
@@ -15,19 +15,45 @@ import ProfilePic from "./ProfilePic";
 import { grey, blue } from "color-name";
 
 const BasicProfile = ({ profile }) => {
+  const [fontLoaded, loadFont] = useState(false);
+
+  async function loadmyFont() {
+    let response = await Expo.Font.loadAsync({
+      'pixel-font': require('../assets/fonts/Muli\-Bold.ttf'),
+    });
+    loadFont(true);
+  }
+
+  useEffect(() => {
+    loadmyFont();
+  }, []);
   return (
     <View style={styles.basicProfile}>
-      <View style={styles.profilePicContainer}>
-        <ProfilePic profile={profile} />
+      {/* <Icon name='camera' size={18} color='rgb(190,190,190)' /> */}
+      <View>
+        <View style={styles.profilePicContainer}>
+          <ProfilePic profile={profile} />
+        </View>
+        {/* <Icon name="edit" size={25} color="black" /> */}
+        {
+          fontLoaded ? (
+            <View style={styles.profileView}>
+              <Text style={styles.profileName}>
+                {profile.first_name} {profile.last_name}
+              </Text>
+            </View>
+          ) : null
+        }
       </View>
-      {/* <Icon name="edit" size={25} color="black" /> */}
-      <View style={styles.profileView}>
-        <Text style={styles.profileName}>
-          {profile.first_name} {profile.last_name}
-        </Text>
-        <Text style={styles.profileText}>{profile.email}</Text>
-        <Text style={styles.profileText}>{profile.age} years old</Text>
-        {/* <Text>{zipcodes.lookup(profile.zip).city}, {zipcodes.lookup(profile.zip).state}</Text> */}
+      <View style={styles.editCoverPhotoContainer}>
+        <View style={styles.editCoverPhotoBackground}>
+          <Icon name='camera' size={20} color='black' />
+        </View>
+      </View>
+      <View style={styles.editProfilePicContainer}>
+        <View style={styles.editProfilePicBackground}>
+          <Icon name='camera' size={20} color='black' />
+        </View>
       </View>
     </View>
   );
@@ -37,27 +63,61 @@ export default BasicProfile;
 
 const styles = StyleSheet.create({
   basicProfile: {
-    flex: 1,
-    justifyContent: "space-evenly",
-    alignItems: "stretch",
-    flexDirection: "row",
+    // justifyContent: "space-evenly",
+    // alignItems: "stretch",
+    // flexDirection: "row",
     width: "100%",
-    backgroundColor: 'rgb(43,49,54)'
+    backgroundColor: 'rgb(62,69,73)',
+    flexDirection: 'row',
+    position: 'relative',
+    justifyContent: 'center'
   },
   profilePicContainer: {
     // backgroundColor: 'grey',
     // paddingRight: 20
   },
   profileView: {
-    justifyContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   profileName: {
     fontSize: 25,
     color: 'rgb(240,240,240)',
+    fontFamily: 'pixel-font',
+    paddingVertical: 15
   },
   profileText: {
     // backgroundColor: 'blue',
     color: 'rgb(240,240,240)',
     // paddingRight: 20
+  },
+  editCoverPhotoContainer: {
+    alignItems: 'flex-end',
+    position: 'absolute',
+    width: '92%',
+    top: -40,
+    paddingBottom: 10,
+    paddingRight: 10
+  },
+  editCoverPhotoBackground: {
+    backgroundColor: 'rgb(190,190,190)',
+    paddingVertical: 4,
+    paddingHorizontal: 7,
+    borderRadius: 5
+  },
+  editProfilePicContainer: {
+    alignItems: 'center',
+    position: 'absolute',
+    width: '92%',
+    paddingTop: 40,
+    paddingLeft: 130
+  },
+  editProfilePicBackground: {
+    backgroundColor: 'rgb(190,190,190)',
+    paddingVertical: 4,
+    paddingHorizontal: 7,
+    borderRadius: 20,
+    borderColor: 'rgb(62,69,73)',
+    borderWidth: 4
   }
 });
