260282a9242b1080618d090d2e6ce07aa62d34aa
Christine Ting <christ123092@yahoo.com>
2020-04-06 17:10:59 -0700

    cleaned up code

--- client/components/FitnessGoals.js
+++ client/components/FitnessGoals.js
@@ -1,13 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, ImageBackground, Picker } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { blockStyle } from '../styles';
 
 const FitnessGoals = ({ profile }) => {
-  const titles = [['Current Weight', 'Weekly Goal'], ['Goal Weight', 'Activity Level']];
-  const dbTitles = [['weight', 'weekly_goal'], ['goal_w', 'activity_lvl']];
+  const titles = [
+    ['Current Weight', 'Weekly Goal'],
+    ['Goal Weight', 'Activity Level']
+  ];
+  const dbTitles = [
+    ['weight', 'weekly_goal'],
+    ['goal_w', 'activity_lvl']
+  ];
+  
   return (
     <View style={styles.main}>
-      { titles.map((title, index) => (
+      {titles.map((title, index) => (
         <View style={styles.column} key={index}>
           <View style={styles.entry}>
             <Text style={styles.itemsTitle}>{title[0]}</Text>
