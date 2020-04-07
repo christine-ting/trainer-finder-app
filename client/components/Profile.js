dac63be532d78affda51893ef4f71c76c7c92211
Christine Ting <christ123092@yahoo.com>
2020-04-06 20:29:47 -0700

    cleaned up code

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,31 +1,24 @@
-import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  ScrollView,
-  Image
-} from "react-native";
-import BasicProfile from "./BasicProfile";
-import PersonalDetails from "./PersonalDetails";
-import FitnessGoals from "./FitnessGoals";
-import WorkoutPlans from "./WorkoutPlans";
-import BMR from "./BMR";
-import Icon from "react-native-vector-icons/Ionicons";
-import { profileStyle } from "../styles";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import BasicProfile from './BasicProfile';
+import PersonalDetails from './PersonalDetails';
+import FitnessGoals from './FitnessGoals';
+import WorkoutPlans from './WorkoutPlans';
+import BMR from './BMR';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { profileStyle } from '../styles';
+import Colors from '../styles/colors';
+
+const mainComponents = [ PersonalDetails, FitnessGoals, WorkoutPlans ];
+const mainTitles = ['Personal Details', 'Fitness Goals', 'Workout Plans'];
 
 const Profile = ({ profile }) => {
-  console.log('PROFILE:', profile)
   const [fontLoaded, loadFont] = useState(false);
-  const [coverPhoto, updateCoverPhoto] = useState({
-    uri:
-      "https://mvpuploadimg.s3-us-west-1.amazonaws.com/Screen+Shot+2020-03-09+at+9.10.16+AM.png"
-  });
+  const [coverPhoto, updateCoverPhoto] = useState({ uri: profile.cover_photo });
 
   async function loadmyFont() {
     let response = await Expo.Font.loadAsync({
-      "pixel-font": require("../../assets/fonts/Muli-Medium.ttf")
+      'pixel-font': require('../../assets/fonts/Muli-Medium.ttf')
     });
     loadFont(true);
   }
@@ -50,47 +43,26 @@ const Profile = ({ profile }) => {
           <View style={styles.BMRContainer}>
             <BMR profile={profile} />
           </View>
-          <View style={styles.sectionContainer}>
-            <View>
-              <View style={styles.sectionTitle}>
-                <View style={styles.iconBackground}>
-                  <Icon name="ios-fitness" size={18} color="rgb(190,190,190)" />
+          { mainComponents.map((Component, index) => (
+            <View style={styles.sectionContainer} key={index}>
+              <View>
+                <View style={styles.sectionTitle}>
+                  <View style={styles.iconBackground}>
+                    <Icon name='ios-fitness' size={18} color={Colors.mediumGrey} />
+                  </View>
+                  <Text style={styles.headerText}>{mainTitles[index]}</Text>
                 </View>
-                <Text style={styles.headerText}>Personal Details</Text>
+                <Component profile={profile} />
               </View>
-              <PersonalDetails profile={profile} />
+              <View style={styles.verticalLine}></View>
             </View>
-            <View style={styles.verticalLine}></View>
-          </View>
-          <View style={styles.sectionContainer}>
-            <View>
-              <View style={styles.sectionTitle}>
-                <View style={styles.iconBackground}>
-                  <Icon name="ios-fitness" size={18} color="rgb(190,190,190)" />
-                </View>
-                <Text style={styles.headerText}>Fitness Goals</Text>
-              </View>
-              <FitnessGoals profile={profile} />
-            </View>
-            <View style={styles.verticalLine}></View>
-          </View>
-          <View style={styles.sectionContainer}>
-            <View>
-              <View style={styles.sectionTitle}>
-                <View style={styles.iconBackground}>
-                  <Icon name="ios-fitness" size={18} color="rgb(190,190,190)" />
-                </View>
-                <Text style={styles.headerText}>Workout Plans</Text>
-              </View>
-              <WorkoutPlans profile={profile} />
-            </View>
-            <View style={styles.shortVerticalLine}></View>
-          </View>
+          )) }  
         </View>
       ) : null}
     </View>
   );
 };
+
 const styles = StyleSheet.create(profileStyle);
 
 export default Profile;
