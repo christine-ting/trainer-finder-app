93747bedc5c048397168ac2a43c4045a435e0001
Christine Ting <christ123092@yahoo.com>
2020-04-12 13:26:30 -0700

    completed error handling for authentication, working on login and sign up successful case

--- client/components/profile/Authentication.js
+++ client/components/profile/Authentication.js
@@ -1,40 +1,98 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Text, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert
+} from 'react-native';
 import { authenticationStyle } from '../../styles/profile';
 import key from '../../env/key';
 import axios from 'axios';
 
+const APIKey = key.firebaseAPIKey;
+
 const Authentication = () => {
-  const [ inputEmail, setEmail ] = useState('');
-  const [ inputPassword, setPassword ] = useState('');
-  const [ showPassword, setShowPassword ] = useState(false); 
-  const [ hiddenPassword, setHiddenPassword ] = useState('');
+  const [inputEmail, setEmail] = useState('');
+  const [inputPassword, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [hiddenPassword, setHiddenPassword] = useState('');
 
   const changeHandler = (text, type) => {
     if (type === 'email') {
       setEmail(text);
     }
     if (type === 'password') {
-      setPassword(text);
-      let count = '';
-      for (var char of text) {
-        count += '*';
+      if (!showPassword) {
+        setPassword(inputPassword + text[text.length - 1]);
+        let count = '';
+        for (var char of text) {
+          count += '*';
+        }
+        setHiddenPassword(count);
+      } else {
+        setPassword(text);
       }
-      setHiddenPassword(count);
     }
   };
 
-  const signUp = () => {
-    const APIKey = key.firebaseAPIKey;
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIKey}`;
+  const authenticate = (method) => {
+    let url;
+    let message;
+    if (method === 'login') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${APIKey}`;
+    }
+    if (method === 'signUp') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIKey}`;
+    }
     const body = {
       email: inputEmail,
       password: inputPassword,
       returnSecureToken: true
     };
-    axios.post(url, body)
-      .then((result) => console.log(result))
-      .catch((err) => console.error(err));
+    axios
+      .post(url, body)
+      .then(result => console.log('result:', result))
+      .catch((error) => {
+        const response = error.response.data.error.message;
+        message = checkError(response);
+        Alert.alert(
+          'Error',
+          message,
+          [
+            {
+              text: 'Okay'
+            }
+          ],
+          { cancelable: false }
+        );
+      });
+  };
+
+  const checkError = (response) => {
+    let message;
+    if (response === 'INVALID_PASSWORD') {
+      message = 'Invalid password.';
+    }
+    if (response === 'INVALID_EMAIL') {
+      message = 'Invalid email.';
+    }
+    if (response.includes('ATTEMPTS')) {
+      message = 'Too many unsuccessful login attempts. Please try again later.';
+    }
+    if (response === 'EMAIL_NOT_FOUND') {
+      message = 'Email not found.';
+    }
+    if (response.includes('WEAK_PASSWORD')) {
+      message = 'Password should be at least 6 characters.';
+    }
+    if (response === 'EMAIL_EXISTS') {
+      message = 'Email exists.';
+    }
+    return message;
   };
 
   return (
@@ -51,31 +109,30 @@ const Authentication = () => {
               onChangeText={text => changeHandler(text, 'email')}
             />
             <Text style={styles.title}>PASSWORD</Text>
-            { showPassword ? (
-              <TextInput
-                style={styles.authInput}
-                value={inputPassword}
-                onChangeText={text => changeHandler(text, 'password')}
-              />
-            ) : (
-              <TextInput
-                style={styles.authInput}
-                value={hiddenPassword}
-                onChangeText={text => changeHandler(text, 'password')}
-              />
-            ) }
-            <TouchableOpacity style={styles.showPwdView} onPress={() => setShowPassword(!showPassword)}>
-              { showPassword ? (
-                <Text style={styles.showPwdText}>hide password</Text>
-              ) : (
-                <Text style={styles.showPwdText}>show password</Text>
-              ) }
+            <TextInput
+              style={styles.authInput}
+              value={showPassword ? inputPassword : hiddenPassword}
+              onChangeText={text => changeHandler(text, 'password')}
+            />
+            <TouchableOpacity
+              style={styles.showPwdView}
+              onPress={() => setShowPassword(!showPassword)}
+            >
+              <Text style={styles.showPwdText}>
+                {showPassword ? 'hide' : 'show'} password
+              </Text>
             </TouchableOpacity>
             <View style={styles.buttonView}>
-              <TouchableOpacity style={styles.loginButton}>
+              <TouchableOpacity
+                style={styles.loginButton}
+                onPress={() => authenticate('login')}
+              >
                 <Text style={styles.buttonText}>LOGIN</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.signUpButton} onPress={() => signUp()}>
+              <TouchableOpacity
+                style={styles.signUpButton}
+                onPress={() => authenticate('signUp')}
+              >
                 <Text style={styles.buttonText}>SIGN UP</Text>
               </TouchableOpacity>
             </View>
